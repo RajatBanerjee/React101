15adfc63e615242ee0d6977140c5a9b443dbea9c
Rajat <rajatmig29@gmail.com>
2015-06-03 10:23:29 -0700

    product model

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -2,8 +2,8 @@ var React = require('react'),
   express = require('express'),
   router = express.Router()
   httpRequest = require('../Utils/HttpRequestModule'),
-  log=  require('../Utils/logger');
-
+  log=  require('../Utils/logger'),
+  productModel = require('../Models/productModel');
   /*
   	router configuration after /Product/*
    */
@@ -12,6 +12,11 @@ var React = require('react'),
   	log.info("--------------arrived here---------")
   	var url ="http://api.art.com/EcommerceAPI.svc/jsonp/CatalogItemGet?apiKey=519BAAC8E607413CA1FC043C92D08AAD&sessionId=A0F866D6907D411395E5CEDB6C359357&itemId=9045049&lookupType=ItemNumber";
   	var req = httpRequest.makeGetRequest(url,null,function(err,data){
+  		//get only the First Item of the aarray
+  		data = data.d.Items[0];
+
+  		//pass the raw data througn a filter to flatten the data
+  		data = productModel.getFlatResponse(data);
   		
   		if(!err){
 			res.render('product', {
