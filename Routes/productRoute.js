7f761b09429cd6bc21ea372d9f2517545bf8c800
Rajat <rajatmig29@gmail.com>
2015-06-03 17:57:13 -0700

    product components + html meta tag

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -4,12 +4,20 @@ var React = require('react'),
   httpRequest = require('../Utils/HttpRequestModule'),
   log=  require('../Utils/logger'),
   productModel = require('../Models/productModel');
+
+var productPageComponent = require('../Components/pp_pageApp.react.js');
   /*
   	router configuration after /Product/*
    */
   
   router.get("/", function(req,res){
-  	log.info("--------------arrived here---------")
+  	log.info("--------------arrived here---------");
+
+
+    var meta= httpRequest.makeGetRequest("http://api.art.com/ECommerceAPI.svc/jsonp/SEOMetaInfoGet?apiKey=519BAAC8E607413CA1FC043C92D08AAD&sessionId=A0F866D6907D411395E5CEDB6C359357&pageSourceType=ProductPage&itemId=9045049&categoryID=6126&pageNumber=1",null,function(err,data){
+      metaTags= data.d.metaTags;
+      
+    });
   	var url ="http://api.art.com/EcommerceAPI.svc/jsonp/CatalogItemGet?apiKey=519BAAC8E607413CA1FC043C92D08AAD&sessionId=A0F866D6907D411395E5CEDB6C359357&itemId=9045049&lookupType=ItemNumber";
   	var req = httpRequest.makeGetRequest(url,null,function(err,data){
   		//get only the First Item of the aarray
@@ -19,8 +27,41 @@ var React = require('react'),
   		data = productModel.getFlatResponse(data);
   		
   		if(!err){
+
+        var titleData ={
+          title: data.title,
+          artist: data.artist,
+          productType:data.productType,
+          ProductPageUrl:data.ProductPageUrl,
+          sku:data.sku
+        }
+        var herodata = data.images;
+        var today = new Date();
+        var tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+
+        var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+        ];
+
+        var bottomBarData ={
+          price:"$"+data.prices.price,
+          arrivesBy: monthNames[tomorrow.getMonth()]+ tomorrow.getDate()
+        };
+      /*
+      renderthe react components
+       */
+      var markup = React.renderComponentToString(
+        productPageComponent({Title: titleData,Hero:herodata,BottomBar: bottomBarData})
+      );
+
 			res.render('product', {
-				markup: JSON.stringify(data,undefined,2)
+        htmlTag: metaTags[0].Value,
+        markup:markup,
+				json: JSON.stringify(data,undefined,2)
 			});
   		}
   		
